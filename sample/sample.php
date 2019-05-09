83c0447018b59b5c1a475672bc0a048a3deff2f7
Torleif Berger <torleif@outlook.com>
2019-05-09 11:25:08 +0200

    Added comments to sample

--- sample/sample.php
+++ sample/sample.php
@@ -1,11 +1,17 @@
 <?php
 
+// Include the class (shouldn't be needed if you use Composer)
 include('../src/Geekality/Transposer.php');
 
+// Get the lyrics with chords
 $text = file_get_contents('sample.txt');
 
+// Parse the text in its original key (in this case, D)
 $song = Geekality\Transposer::parse($text, 'D');
+
+// If we have a key to transpose to
 if(isset($_GET['key']))
+    // Transpose it to that key
     $song->transpose($_GET['key']);
 
 ?>
@@ -59,8 +65,10 @@ if(isset($_GET['key']))
 </head>
 <body>
 
+// Optionally output HTML for a key selector
 <?php echo $song->get_key_selector('sample.php?key=') ?>
 
+// Output the transposed song
 <?php echo $song ?>
 
 </body>
