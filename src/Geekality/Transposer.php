7af3a79935aff66a3ee17d2be542c9910fa77058
Eljay <jakepucan@gmail.com>
2021-09-25 17:38:29 -0400

    Added other naming convention for chords

--- src/Geekality/Transposer.php
+++ src/Geekality/Transposer.php
@@ -320,7 +320,7 @@ class Transposer_ChordLine
 }
 class Transposer_Chord
 {
-	public static $pattern = '%(\s*+\/?)([A-H][♯♭b\#]?)((?:2|5|6|7|9|11|13|6\/9|7\-5|7\-9|7\#5|7\#9|7\+5|7\+9|7b5|7b9|7sus2|7sus4|add2|add4|add9|aug|dim|dim7|m\/maj7|m6|m7|m7b5|m9|m11|m13|maj7|maj9|maj11|maj13|mb5|m|sus4|sus2|sus)*)(\s*+)%u';
+	public static $pattern = '%(\s*+\/?)([A-H][♯♭b\#]?)((?:2|5|6|7|9|11|13|6\/9|7\-5|7\-9|7\#5|7\#9|7\+5|7\+9|7b5|7b9|7sus2|7sus4|add2|add4|add9|aug|dim|dim7|m\/maj7|m6|m7|min7|m7b5|m9|min9|m11|min11|m13|maj7|M7|maj9|maj11|maj13|mb5|m|sus4|sus2|sus)*)(\s*+)%u';
 
 	private $text;
 	private $pre;
